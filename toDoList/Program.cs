e02872cc3c6cf8aaa8f7f9920b37764c8ed7a71c
Glogzi <116293555+Glogzi@users.noreply.github.com>
2024-01-11 19:19:44 +0100

    bug fix
    fixed bug where program crushed when file toDo.toDo did not existed

--- toDoList/Program.cs
+++ toDoList/Program.cs
@@ -1,20 +1,15 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace toDoList {
     internal class Program {
         static string response(string[] msg) {
-            int id = 0;
-            try {
-                string[] tempList = File.ReadAllLines("toDo.txt");
-                for (int i = 0; i < tempList.Length; i++) {
-                    id++;
-                }
+            int id = 1;
+            string[] allList = new string[1];
+            allList = File.ReadAllLines("toDo.toDo");
+            for (int i = 0; i < allList.Length; i++) {
+                id++;
             }
-            catch {
-                File.Create("./toDo.txt");
-                id = 1;
-            }
-            string[] allList = File.ReadAllLines("toDo.txt");
             string strID = Convert.ToString(id);
             switch (msg[0].ToLower()) {
                 case "help":
@@ -23,11 +18,16 @@ namespace toDoList {
                     return extraCommands.add(msg, strID);
                 case "show":
                     return extraCommands.show(allList);
+                case "mark":
+                    return extraCommands.mark(msg, allList, "mark");
+                case "unmark":
+                    return extraCommands.mark(msg, allList, "unmark");
                 default:
                     return "command not found";
             }
         }
         static void Main() {
+            extraCommands.create();
             Console.WriteLine("welcome to console toDo list");
             Console.WriteLine("write help to get help with commands");
             while (true){
@@ -39,16 +39,24 @@ namespace toDoList {
         }
     }
     public class extraCommands {
+        public static void create() {
+            if (!File.Exists("./toDo.toDo")){
+                File.Create("./toDo.toDo");
+                Process.Start("./toDoList.exe");
+                Environment.Exit(0);
+            }
+            return;
+        }
         public static string add(string[] msg, string id) {
             foreach (string word in msg) {
                 if (word != msg[0]) {
-                    File.AppendAllText("toDo.txt", word + " ");
+                    File.AppendAllText("toDo.toDo", word + " ");
                     continue;
                 }
-                File.AppendAllText("toDo.txt", id + ". ");
+                File.AppendAllText("toDo.toDo", id + ". ");
             }
 
-            File.AppendAllText("toDo.txt", "\n");
+            File.AppendAllText("toDo.toDo", "\n");
             return "added to an toDo list";
         }
         public static string help() {
@@ -63,6 +71,16 @@ namespace toDoList {
                 Console.WriteLine(line);
             }
             return "";
+        }
+        public static string mark(string[] msg, string[] list, string mode) {
+            string red = "\x1b[31m";
+            string reset = "\x1b[0m";
+            int line = Convert.ToInt32(msg[1]);
+            if (mode == "mark") {
+                list[line] = red + list[line] + reset;
+            }
+            File.WriteAllLines("./toDo.toDo", list);
+            return $"marked line with id {line}";
         }
     }
 }
